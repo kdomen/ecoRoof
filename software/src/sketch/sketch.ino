4b1db4a4424aef6b77e37f3f802e908da80504b6
Kyle Domen <kdomen99@gmail.com>
2016-02-28 22:44:05 -0800

    pump you up

--- software/src/sketch/sketch.ino
+++ software/src/sketch/sketch.ino
@@ -8,7 +8,19 @@ void setup() {
     lcd.setCursor(0, 1); lcd.print("Moisture: ");     // set cursor at (10, 1) to edit
     lcd.setCursor(0, 2); lcd.print("Temperature: ");  // set cursor at (13, 2) to edit
     lcd.setCursor(0, 3); lcd.print("Water Level: ");  // set cursor at (13, 3) to edit
+
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
 }
 
 void loop() {
+    digitalWrite(3, HIGH);
+    delay(750);
+    digitalWrite(3, LOW);
+    delay(750);
+
+    digitalWrite(4, HIGH);
+    delay(750);
+    digitalWrite(4, LOW);
+    delay(750);
 }
