30bc408a115de631261504a3a53a1168435d7572
Kyle Domen <kdomen99@gmail.com>
2016-02-28 17:21:04 -0800

    add LCD beginnings

--- software/src/sketch.ino
+++ software/src/sketch.ino
@@ -4,9 +4,11 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
     lcd.begin(16, 4);
-    lcd.print("hello, world!");
+    lcd.print("Status: ");                            // set cursor at (8, 0) to edit
+    lcd.setCursor(0, 1); lcd.print("Moisture: ");     // set cursor at (10, 1) to edit
+    lcd.setCursor(0, 2); lcd.print("Temperature: ");  // set cursor at (13, 2) to edit
+    lcd.setCursor(0, 3); lcd.print("Water Level: ");  // set cursor at (13, 3) to edit
 }
 
 void loop() {
 }
-
