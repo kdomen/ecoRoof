1886f89715fd5dfb84176e0971774cbb5cd1d1c5
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-03-05 18:08:32 -0800

    set contrast and bl pins

--- software/src/lcd.cpp
+++ software/src/lcd.cpp
@@ -11,8 +11,13 @@
 LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 void lcd_init() {
-    lcd.begin(16, 4);
+    /* setup contrast and backlight */
+    pinMode(LCD_CONTRAST,  OUTPUT);
+    pinMode(LCD_BACKLIGHT, OUTPUT);
+    analogWrite(LCD_CONTRAST,  (1.57) /5*255);
+    analogWrite(LCD_BACKLIGHT, (3.20) /5*255);
 
+    lcd.begin(16, 4);
     lcd.print("Status: ");
     lcd.setCursor(0, 1); lcd.print("Moisture: ");
     lcd.setCursor(0, 2); lcd.print("Temperature: ");
