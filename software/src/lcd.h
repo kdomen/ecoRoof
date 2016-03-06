651c886a1316f696e66f32502e010963488dba63
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-03-05 17:46:27 -0800

    cleanup

--- software/src/lcd.h
+++ software/src/lcd.h
@@ -7,4 +7,6 @@
 #ifndef LCD_H
 #define LCD_H
 
+void lcd_init();
+
 #endif /* LCD_H */
--- software/src/sketch.ino
+++ software/src/sketch.ino
@@ -2,19 +2,9 @@
 #include "lcd.h"
 #include "pump.h"
 
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
 void setup() {
-    lcd.begin(16, 4);
-    lcd.print("Status: ");                            // set cursor at (8, 0) to edit
-    lcd.setCursor(0, 1); lcd.print("Moisture: ");     // set cursor at (10, 1) to edit
-    lcd.setCursor(0, 2); lcd.print("Temperature: ");  // set cursor at (13, 2) to edit
-    lcd.setCursor(0, 3); lcd.print("Water Level: ");  // set cursor at (13, 3) to edit
-
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
+    lcd_init();
 }
 
-void setup() {
-    pinMode(SIGNAL_PIN, OUTPUT);
+void loop() {
 }
