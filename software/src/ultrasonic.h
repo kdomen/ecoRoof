e86cf6c26dc6ee6844e19abc21b3e8930044214e
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-03-27 20:11:45 -0700

    change height

--- software/src/ultrasonic.h
+++ software/src/ultrasonic.h
@@ -7,7 +7,8 @@
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
 
-#define RESV_HEIGHT 4 * 2.54 // 4 inches -> cm
+//#define RESV_HEIGHT 4 * 2.54 // 4 inches -> cm
+#define RESV_HEIGHT 3.75 * 2.54
 
 void ultrasonic_init();
 float read_water_level();
