bca073925210fabb6721b3ef902badfcca8496ef
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-04-01 23:03:27 -0700

    fix func defintion

--- software/src/lcd.ino
+++ software/src/lcd.ino
@@ -12,7 +12,7 @@
 
 LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
-void lcd_init(contrast) {
+void lcd_init(float contrast) {
     /* setup contrast and backlight */
     pinMode(LCD_CONTRAST,      OUTPUT);
     pinMode(LCD_BACKLIGHT,     OUTPUT);
