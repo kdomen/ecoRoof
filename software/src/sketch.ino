8fb59700bdcf9d90679bf8e3629783972b68d9fc
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-04-01 23:30:48 -0700

    update LCD less

--- software/src/sketch.ino
+++ software/src/sketch.ino
@@ -95,12 +95,12 @@ void loop() {
             else if (running_pump == RESV_PUMP) lcd_update_status("Irrigating... " + cd);
         }
 
-        // temp
-        lcd_update_temp(read_temp());
-
-        // water level
+        // temp and water level
         float wl = read_water_level();
-        lcd_update_water_level(wl);
+        if (tick % 10 == 0) {
+            lcd_update_water_level(wl);
+            lcd_update_temp(read_temp());
+        }
         matrix_graph(wl);
 
         // humidity
